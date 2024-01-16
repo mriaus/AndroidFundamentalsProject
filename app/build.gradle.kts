62d8603f1f083be376383641560d004614b8914a
Marcos Riau <riau222@gmail.com>
2024-01-16 17:22:52 +0100

    WIP: Api calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,18 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    //okHTTP
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.10.0")
+    //GSON
+    implementation("com.google.code.gson:gson:2.10.1")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/personalsProjects/androidfundamentalsproject/MainActivity.kt
+++ app/src/main/java/com/personalsProjects/androidfundamentalsproject/MainActivity.kt
@@ -3,10 +3,17 @@ package com.personalsProjects.androidfundamentalsproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.lifecycleScope
 import com.personalsProjects.androidfundamentalsproject.characterList.ActivityCharactersList
 import com.personalsProjects.androidfundamentalsproject.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +23,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button.setOnClickListener({viewModel.onPressLogin()
-            ActivityCharactersList.goToCharactersList(this)
+        setObservers()
+        binding.button.setOnClickListener({viewModel.onPressLogin("","")
+           // ActivityCharactersList.goToCharactersList(this)
         })
         binding.editTextTextEmailAddress.doAfterTextChanged {  }
         binding.editTextTextPassword.doAfterTextChanged {  }
     }
+
+    private fun setObservers() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.uiState.collect { state ->
+                    when (state) {
+                        is MainActivityViewModel.State.Idle -> idle()
+                        is MainActivityViewModel.State.Error -> showError(state.message)
+                        is MainActivityViewModel.State.Loading -> showLoading(true)
+                        is MainActivityViewModel.State.SuccesLogin -> showSuccesLogin()
+                    }
+            }
+        }
+
+    }
+
+    private fun showSuccesLogin() {
+       // viewModel.onPressLogin("","")
+       // ActivityCharactersList.goToCharactersList(this)
+    }
+
+    private fun showLoading(show: Boolean) {
+        Log.println(Log.INFO,"Entra en showLoading()", "${show}")
+
+        if (show) {
+            binding.button.visibility = View.GONE
+            binding.pbLoading.visibility = View.VISIBLE
+        }
+        else{
+            binding.pbLoading.visibility = View.GONE
+            binding.button.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    private fun idle(){
+        Log.println(Log.INFO,"Entra en Idle()", "")
+
+        // showLoading(false)
+    }
+
 }
